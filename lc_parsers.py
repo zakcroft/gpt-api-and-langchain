823a6b43160ca2dfccd28dabf05edff0765c331d
zak <zakcroft@gmail.com>
2023-06-21 09:59:43 +0100

    print

--- lc_parsers.py
+++ lc_parsers.py
@@ -5,8 +5,11 @@ from langchain.chains import LLMChain
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
+
 openai.api_key = os.environ["OPENAI_API_KEY"]
 os.environ["OPENAI_API_KEY"]
 
 llm = OpenAI(model_name="text-ada-001")
-llm("Tell me a joke")
+output = llm("Tell me a joke")
+print(output)
+print(repr(output)) # with new lines
