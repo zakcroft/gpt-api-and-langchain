068d58e26d80f5852a7cf7ee67050f7fdb5ffce1
zak <zakcroft@gmail.com>
2023-06-22 22:05:03 +0100

    memory chain

--- lc_indexes.py
+++ lc_indexes.py
@@ -1,6 +1,7 @@
 import os
 import pinecone
-
+import json
+import pprint
 from dotenv import load_dotenv, find_dotenv
 
 from _setup import print_to_pretty_json
@@ -33,11 +34,14 @@ from langchain.chains import RetrievalQA, ConversationalRetrievalChain
 load_dotenv(find_dotenv())
 
 index_name = "langchain-tut"
+
+# settings ===
 populate_vector_store = False
 pinecone.init(
     api_key=os.environ["PINECONE_API_KEY"],
     environment=os.environ["PINECONE_ENV"],
 )
+# ===
 
 if (populate_vector_store):
     loader = DirectoryLoader(
@@ -82,50 +86,33 @@ PROMPT = ChatPromptTemplate.from_template(
 chain_type_kwargs = {"prompt": PROMPT}
 
 # chat = RetrievalQA.from_chain_type(llm=llmChat, chain_type="stuff",
-#
 
-
-def get_chat_history(chat_history) -> str:
-    buffer = ""
-    print('List', chat_history)
-    # print('CHAT_TURN_TYPE', CHAT_TURN_TYPE)
-    # print('LiList[CHAT_TURN_TYPE]st', List[CHAT_TURN_TYPE])
-    # for dialogue_turn in chat_history:
-    #     if isinstance(dialogue_turn, BaseMessage):
-    #         role_prefix = _ROLE_MAP.get(dialogue_turn.type, f"{dialogue_turn.type}: ")
-    #         buffer += f"\n{role_prefix}{dialogue_turn.content}"
-    #     elif isinstance(dialogue_turn, tuple):
-    #         human = "Human: " + dialogue_turn[0]
-    #         ai = "Assistant: " + dialogue_turn[1]
-    #         buffer += "\n" + "\n".join([human, ai])
-    #     else:
-    #         raise ValueError(
-    #             f"Unsupported chat history format: {type(dialogue_turn)}."
-    #             f" Full chat history: {chat_history} "
-    #         )
-    # return buffer                                 retriever=vector_store.as_retriever(), chain_type_kwargs=chain_type_kwargs,)
-
-
-memory = ConversationSummaryBufferMemory(
-    llm=llmChat, max_token_limit=100, memory_key="chat_history",)
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
 chat = ConversationalRetrievalChain.from_llm(
     llm=llmChat,
     memory=memory,
     retriever=vector_store.as_retriever(),
-    combine_docs_chain_kwargs={"prompt": PROMPT},
-    get_chat_history=get_chat_history
+    combine_docs_chain_kwargs=chain_type_kwargs,
+    # get_chat_history=lambda h:h,
+#     get_chat_history=get_chat_history
+    verbose=True
 )
 
 query = "Do you offer vegan food?"
-out = chat({"question": query})
+chat({"question": query})
+
+# print(memory)
+
 chat({"question": "How much does it cost?"})
 
+out = chat({"question": "How about a Seafood Platter"})
 # query_chat = "When does the restaurant open?"
 # out = chat.run(query_chat)
 
-print(out)
-print(memory)
+# print('out====', json.dumps(out, indent=4))
+# print(memory)
+# print(out)
 
 # query_chat2 = "What gluten free options do you have?"
 # out2 = chat.run(query_chat2)
